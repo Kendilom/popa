2f3e8cd9e279c738c6c81d8150e2f48e241ba4aa
Kendilom_ <25032008vlad@gmail.com>
2026-09-23 15:21:23 +0300

    Внесены изминения

--- lesson_8/homework_08.py
+++ lesson_8/homework_08.py
@@ -6,8 +6,8 @@ class Student:
 
     def change_avg_score(self, new_avg_score = int):
         self.score = new_avg_score
-
+    def __repr__(self):
+        return f'Student {self.name}, {self.score}, {self.age}'
 student = Student('Vova', 20, 90)
-print()
 student.change_avg_score(100)
-print(student.score)
+print(student)
